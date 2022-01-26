61c212e5142d9e463de23ac5f359daaa9d19a535
Ismael <imosquerad@gmail.com>
2022-01-26 12:31:16 -0400

    primer commit del proyecto

---
+++ procesar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        print("Su Nombre es: ".$_POST["nombre"]."<br>");
+	print("Su Apellido es: ".$_POST["apellido"]);
+        // put your code here
+        ?>
+    </body>
+</html>
